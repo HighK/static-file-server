e91e8379250975a19786cca2e1a772be411b5c31
highK <restartplant@gmail.com>
2019-11-30 21:59:27 +0900

    cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use("/static", express.static(__dirname + "/test"));
 
 app.listen(4000, function() {
